e40c374a6fd83a9323f0e2017d9da4e90b381d31
Johnny Proano <jproano22@yahoo.com>
2023-03-04 01:17:54 -0500

    added responsive nav bar for mobile navToggleClick and handle NavClose

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,45 +1,79 @@
 import "./Header.css";
 import { LogoutLink } from "./LogoutLink";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 export function Header() {
+  const navToggleClick = () => {
+    document.body.classList.toggle("nav-open");
+  };
+
+  const handelNavClose = () => {
+    document.body.classList.remove("nav-open");
+  };
+
   return (
     <header>
-      <div className="nav">
+      <div className="navbar">
         <div className="logo">
           <h3>BrewR</h3>
         </div>
-        <div className="nav-menu">
-          <Link to="/">
-            <img src="/images/home-house-building-svgrepo-com.svg" alt="HOME" />
-            <span> HOME</span>
-          </Link>
 
-          <Link to="/tours/index">
-            <img src="/images/pedestrian-walk-svgrepo-com.svg" alt="WATCHLIST" />
-            <span> TOURS</span>
-          </Link>
+        <button
+          // type="submit"
+          onClick={navToggleClick}
+          className="nav-toggle"
+          // id="toggle"
+          aria-label="toggle navigation"
+        >
+          <span className="hamburger"></span>
+        </button>
 
-          <Link to="/brewery/index">
-            <img src="/images/beer-svgrepo-com.svg" alt="SEARCH" />
-            <span>SEARCH BREWERY</span>
-          </Link>
+        <nav className="nav">
+          <div className="menu-list">
+            <Link to="/">
+              <img src="/images/home-house-building-svgrepo-com.svg" alt="HOME" />
+              <span className="span-link" onClick={handelNavClose}>
+                {" "}
+                HOME
+              </span>
+            </Link>
 
-          {localStorage.jwt === undefined ? (
-            <>
-              <Link to="/signup">
-                <img src="/images/user-svgrepo-com.svg" alt="HOME" />
-                <span>SIGNUP</span>
-              </Link>
-              <Link to="/login">
-                <img src="/images/login-svgrepo-com.svg" alt="HOME" />
-                <span>LOGIN</span>
-              </Link>
-            </>
-          ) : (
-            <LogoutLink />
-          )}
-        </div>
+            <Link to="/tours/index">
+              <img src="/images/pedestrian-walk-svgrepo-com.svg" alt="WATCHLIST" />
+              <span className="span-link" onClick={handelNavClose}>
+                {" "}
+                TOURS
+              </span>
+            </Link>
+
+            <Link to="/brewery/index">
+              <img src="/images/beer-svgrepo-com.svg" alt="SEARCH" />
+              <span className="span-link" onClick={handelNavClose}>
+                SEARCH BREWERY
+              </span>
+            </Link>
+
+            {localStorage.jwt === undefined ? (
+              <>
+                <Link to="/signup">
+                  <img src="/images/user-svgrepo-com.svg" alt="HOME" />
+                  <span className="span-link" onClick={handelNavClose}>
+                    SIGNUP
+                  </span>
+                </Link>
+                <Link to="/login">
+                  <img src="/images/login-svgrepo-com.svg" alt="HOME" />
+                  <span className="span-link" onClick={handelNavClose}>
+                    LOGIN
+                  </span>
+                </Link>
+              </>
+            ) : (
+              <LogoutLink />
+            )}
+          </div>
+        </nav>
       </div>
     </header>
   );
